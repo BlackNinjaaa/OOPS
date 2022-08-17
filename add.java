73baebb9acc3211099643c2bb72f52f515b9aa64
BlackNinjaaa <suriyasurya792@gmail.com>
2022-08-17 17:58:02 +0530

    add

---
+++ add.java
@@ -0,0 +1,18 @@
+public class add{
+   int a;
+   int b;
+   String c;
+   
+void alphabet()
+{
+  System.out.println("value a is:"+a+ "\n"+"value b is:"+b +"\n"+ "value c is:"+c +"\n");
+}
+public static void main(String args[]){
+
+add surya= new add();
+surya.a=28;
+surya.b=33;
+surya.c="ninja";
+surya.alphabet();
+}
+}
